df16e72e0ddf9a3b16c98351ba7c72a890b9c5d0
Thomas De Oliveira <thomas.deoliveira.pro@outlook.fr>
2025-02-25 09:50:49 +0100

    ✨ Add Terraform configuration and lock file for random string generation

---
+++ Atelier 1/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+
+resource "random_string" "example" {
+  length  = 20
+  special = false
+  upper   = true
+  lower   = true
+  numeric = true
+}
+
+resource "local_file" "example_file" {
+  filename = "./random_string.txt"
+  content  = random_string.example.result
+}
+
+output "generated_random_string" {
+  value       = random_string.example.result
+  description = "This is the randomly generated string displayed on the screen"
+}
+
+resource "local_file" "bonus_file" {
+  filename = "bonus.txt"
+  content  = file("C:/Windows/System32/drivers/etc/hosts")
+} 
